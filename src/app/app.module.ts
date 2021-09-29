beb0aaafcb085b65ec9edb978641bcdc15ce9f2d
ojuara-B <deividaraujope@gmail.com>
2021-09-29 14:35:58 -0300

    Menu lateral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,27 @@ import { HeaderComponent } from './components/templates/header/header.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { FooterComponent } from './components/templates/footer/footer.component';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import { NavComponent } from './components/templates/nav/nav.component';
+import {MatSidenavModule} from '@angular/material/sidenav'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
